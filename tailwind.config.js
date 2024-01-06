b10ce7e50fb4b3cfbdc3858e57253f2365fe11e2
payalmulani402 <payalmulani402@gmail.com>
2024-01-06 16:33:11 +0530

    6-1-2024

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        blink: {
+          '0%': { opacity: 0 },
+          '50%': { opacity: 0.5 },
+          '100%': { opacity: 1 }
+        }
+      },
+      animation: {
+        blink: 'blink 0.5s ease-in-out'
+      }
+    },
+  },
+  plugins: [],
+}
